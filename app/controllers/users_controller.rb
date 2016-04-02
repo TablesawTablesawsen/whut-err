d8e58f2dfac2f71de7695bd6acff3510daee62ff
Tablesaw Tablesawsen <tablesaw@gmail.com>
2016-04-01 17:54:31 -0700

    Add timeline to show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,12 @@ class UsersController < ApplicationController
   end
 
   def show
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["YOUR_CONSUMER_KEY"]
+      config.consumer_secret     = ENV["YOUR_CONSUMER_SECRET"]
+    end
+    @user = client.user(params[:id])
+    @timeline = client.user_timeline(params[:id])
+
   end
 end
