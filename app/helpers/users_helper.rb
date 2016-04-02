53b4715dce0a46a6113eecc63c2fdcb4fa5cccfc
Tablesaw Tablesawsen <tablesaw@gmail.com>
2016-04-01 19:34:35 -0700

    Add links for user mentions

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,17 @@
 module UsersHelper
+
+  def text_with_user_mentions (tweet)
+    text_with_mentions = tweet.full_text.dup
+    if tweet.user_mentions?
+      tweet.user_mentions.reverse_each do |mention|
+        mention_range = mention.indices[0]...mention.indices[1]
+        text_with_mentions[mention_range] = link_to(
+          text_with_mentions[mention_range],
+          user_path(mention.screen_name)
+          )
+      end
+    end
+    text_with_mentions
+  end
+
 end
