3f8f597a8aa5d8f91136daf3107a97a503292112
Tablesaw Tablesawsen <tablesaw@gmail.com>
2016-04-04 18:33:08 -0700

    show retweets separately

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,12 @@
 module UsersHelper
 
   def text_with_user_mentions (tweet)
+    tweet = tweet.retweeted_status if tweet.retweet?
+
     text_with_mentions = sanitize(tweet.full_text.dup)
     if tweet.user_mentions?
       tweet.user_mentions.reverse_each do |mention|
         user = @client.user(mention.screen_name)
-        # This method relies on the accuracy of the initial indices value from
-        # the API call, but this doesn't appear to always be accurate (though
-        # it seems more reliable than the ending index)
         mention_range = mention.indices[0]..mention.indices[0] + user.screen_name.length
         text_with_mentions[mention_range] = link_to(
           text_with_mentions[mention_range],
